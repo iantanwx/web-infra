c1ba734605b214578181ec47444c6c1914752a95
Ian Tan <ian.tan@go-jek.com>
2019-10-28 21:57:16 +0800

    chore(gcp): move bastion startup script to common env

--- terraform/gcp/environments/stg/bastion.tf
+++ terraform/gcp/environments/stg/bastion.tf
@@ -1,12 +1,12 @@
 module "jump_box" {
-  source        = "../../modules/network/modules/bastion-host"
-  project       = google_project.project.project_id
-  instance_name = "${var.host_project_name}-jump-box"
-  subnetwork    = module.stg_app_vpc.public_subnetwork
-  zone          = "asia-southeast1-a"
-  tag           = "public-restricted"
-  static_ip     = google_compute_address.jump_box_ip.address
-  startup_script = file("${path.module}/files/bastion-startup.sh")
+  source                = "../../modules/network/modules/bastion-host"
+  project               = google_project.project.project_id
+  instance_name         = "${var.host_project_name}-jump-box"
+  subnetwork            = module.stg_app_vpc.public_subnetwork
+  zone                  = "asia-southeast1-a"
+  tag                   = "public-restricted"
+  static_ip             = google_compute_address.jump_box_ip.address
+  startup_script        = file("${path.module}/../common/files/bastion-startup.sh")
   service_account_email = google_service_account.k8s_admin.email
 }
 
