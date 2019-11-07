62633ade6156fba288160ca69b965858eb7682c9
Ian Tan <ian.tan@go-jek.com>
2019-11-07 23:21:39 +0800

    feat(gcp): allow only ssh traffic to bastion hosts

--- terraform/gcp/environments/stg/bastion.tf
+++ terraform/gcp/environments/stg/bastion.tf
@@ -4,7 +4,7 @@ module "jump_box" {
   instance_name          = "${var.host_project_name}-jump-box"
   subnetwork             = module.stg_app_vpc.public_subnetwork
   zone                   = "asia-southeast1-a"
-  tag                    = "public-restricted"
+  tag                    = "ssh"
   static_ip              = google_compute_address.jump_box_ip.address
   startup_script         = file("${path.module}/../common/files/bastion-startup.sh")
   service_account_email  = google_service_account.k8s_admin.email
